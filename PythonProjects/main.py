dba20e811bf081b8bff965f2bbf8a5791f600307
Vladimirset <agentmuv@yandex.ru>
2025-05-04 15:10:11 +0400

    Add files via upload

---
+++ PythonProjects/main.py
@@ -0,0 +1,28 @@
+import requests
+
+URL = 'https://api.pokemonbattle.ru/v2'
+TOKEN = '**************'
+HEADER = {'Content-Type':'application/json', 'trainer_token':TOKEN}
+
+body_create = {
+    "name": "Бульон",
+    "photo_id": 979
+}
+body_change = {
+    "pokemon_id": "307806",
+    "name": "Кампот",
+    "photo_id": 2
+}
+
+body_caught = {
+    "pokemon_id": "307806"
+}
+
+'''response = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_create,)
+print (response.status_code)'''
+
+'''response_change = requests.put(url = f'{URL}/pokemons', headers = HEADER, json = body_change,)
+print (response_change.status_code)'''
+
+'''response_change = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_change,)
+print (response_change.status_code)'''
